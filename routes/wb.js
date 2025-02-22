191d836a5f0f0c7a6d0061da8be9acda144154e1
sathizz1781 <104371331+sathizz1781@users.noreply.github.com>
2025-02-23 00:18:52 +0530

    Update wb.js

--- routes/wb.js
+++ routes/wb.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const fs = require("fs");
+const router = express.Router();
+const path = require("path");
+const { getLastBill } = require("../modules/wb");
 
+router.get("/getlastbill", getLastBill);
+
+module.exports = router;
