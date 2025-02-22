a4fc055eddaed69c7d6a3ca57283fbef0dcaff44
sathizz1781 <104371331+sathizz1781@users.noreply.github.com>
2025-02-22 23:35:23 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,10 @@ const express = require("express");
 const fs = require("fs");
 const router = express.Router();
 const path = require("path");
-const { createUser, userList } = require("../modules/user");
+const { createUser, userList,updateUser } = require("../modules/user");
 
 router.post("/createuser", createUser);
+router.post("/updateuser", updateUser);
 router.get("/userlist", userList);
 
 module.exports = router;
