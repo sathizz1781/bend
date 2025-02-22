adc66e0f1b115d86509bff0caf745a6dc930d890
sathizz1781 <104371331+sathizz1781@users.noreply.github.com>
2025-02-23 00:26:51 +0530

    Update wb.js

--- modules/wb.js
+++ modules/wb.js
@@ -1,1 +1,11 @@
+const getLastBill = async (req, res) => {
+  try {
+    const users = await User.find().sort({ customerId: -1 });
 
+    res.status(200).json({ message: "Users retrieved successfully", users });
+  } catch (error) {
+    console.error("Error getting users:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+module.exports = { getLastBill };
