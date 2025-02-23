cf65efe46f1bfac3a42160bc9a1a3d8de19c3279
sathizz1781 <104371331+sathizz1781@users.noreply.github.com>
2025-02-23 12:42:11 +0530

    Update wb.js

--- modules/wb.js
+++ modules/wb.js
@@ -18,19 +18,23 @@ const getLastBill = async (req, res) => {
   }
 };
 const getPrevWeightOfVehicle = async (req, res) => {
-  const collection = mongoose.connection.db.collection("wb");
+  
   try {
-console.log(req.body)
-    let {vehicleNo} = req.body
-    const lastRecord = await mongoose.connection.db
+console.log(req.body);
+    const vehicleNo = req.body.vehicleNo;
+    
+    // Retrieve all records for the given vehicleNo sorted descending by sl_no
+    const records = await mongoose.connection.db
       .collection("wb")
-      .find({vehicle_no:vehicleNo}, { sort: { sl_no: -1 } }); 
+      .find({ vehicle_no: vehicleNo })
+      .sort({ sl_no: -1 })
+      .toArray();
 
-    if (!lastRecord) {
+    if (!records || records.length === 0) {
       return res.status(404).json({ message: "No records found" });
     }
 
-    res.status(200).json({ message: "Last Weight list", data: lastRecord });
+    res.status(200).json({ message: "Last Weight list", data: records });
   } catch (error) {
     console.error("Error getting users:", error);
     res.status(500).json({ message: "Internal server error" });
