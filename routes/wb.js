cf3ea5139b72170edbb1e4306f65b6c9e4de1767
sathizz1781 <104371331+sathizz1781@users.noreply.github.com>
2025-02-23 18:06:00 +0530

    Update wb.js

--- routes/wb.js
+++ routes/wb.js
@@ -2,10 +2,11 @@ const express = require("express");
 const fs = require("fs");
 const router = express.Router();
 const path = require("path");
-const { getLastBill ,getPrevWeightOfVehicle,getRecords} = require("../modules/wb");
+const { getLastBill ,getPrevWeightOfVehicle,getRecords,getCharges} = require("../modules/wb");
 
 router.get("/getlastbill", getLastBill);
 router.post("/getprevweightofvehicle", getPrevWeightOfVehicle);
 router.post("/getrecords", getRecords );
+router.get("/charges", getCharges );
 
 module.exports = router;
